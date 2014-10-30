861ea434a1204984608bc1533707e454ee215d56
Marko Heijnen <info@markoheijnen.com>
2014-10-30 10:25:26 -0700

    Add card structure.

---
+++ cards.php
@@ -0,0 +1,12 @@
+<?php
+
+$GLOBALS['cards'] = array(
+	array(
+		'reading_card' => __(),
+		'playing_card' => __(),
+		'meta' => array(
+			'plugin_slug'    => '',
+			'code_reference' => '',
+		),
+	),
+);
