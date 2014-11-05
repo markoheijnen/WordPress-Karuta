c2e1d50d86a1e2f142905a21495a036d9aa5072b
Marko Heijnen <info@markoheijnen.com>
2014-11-05 12:59:17 +0100

    Update default card deck. Fixes #1.

--- card-decks/default.php
+++ card-decks/default.php
@@ -2,8 +2,8 @@
 $cards = array (
 	 
 	array (
-		'reading_card' => __( 'ページナビゲーション（ページ送り）を作成するプラグイン', 'karuta' ),
-		'getting_card' => __( 'WP-PageNavi', 'karuta' ),
+		'reading_card' => __( 'A plugin that helps build page navigation.', 'karuta' ),
+		'getting_card' => 'WP-PageNavi',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -11,8 +11,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'テーマの品質に対するガイドラインに適合しているかどうかをチェックするためのプラグイン', 'karuta' ),
-		'getting_card' => __( 'Theme Check', 'karuta' ),
+		'reading_card' => __( 'The plugin that checks whether a theme complies with theme review guidelines.', 'karuta' ),
+		'getting_card' => 'Theme Check',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -20,8 +20,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'ユーザー権限を細かくカスタマイズできるプラグイン', 'karuta' ),
-		'getting_card' => __( 'User Role Editor', 'karuta' ),
+		'reading_card' => __( 'The plugin that helps customize user permissions further.', 'karuta' ),
+		'getting_card' => 'User Role Editor',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -29,8 +29,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '表示中のページで利用しているテンプレートファイル名を管理バーに表示するプラグイン', 'karuta' ),
-		'getting_card' => __( 'Show Current Template', 'karuta' ),
+		'reading_card' => __( 'The plugin that displays the template file name used in the current page within the admin bar.', 'karuta' ),
+		'getting_card' => 'Show Current Template',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -38,8 +38,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '項目をショートコードで記述する確認画面付きフォームプラグイン', 'karuta' ),
-		'getting_card' => __( 'MW WP Form', 'karuta' ),
+		'reading_card' => __( 'The plugin for inquiry forms with the confirmation screen. We can use a short cord to describe an item.', 'karuta' ),
+		'getting_card' => 'MW WP Form',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -47,8 +47,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '管理画面の投稿記事一覧画面でドラッグアンドドロップでの並び替えを行うプラグイン', 'karuta' ),
-		'getting_card' => __( 'Intuitive Custom Post Order', 'karuta' ),
+		'reading_card' => __( 'The plugin that enables you to rearrange posts via drag & drop within WP-Admin.', 'karuta' ),
+		'getting_card' => 'Intuitive Custom Post Order',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -56,8 +56,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '日本発のカスタムフィールドの入力を最適化するプラグイン', 'karuta' ),
-		'getting_card' => __( 'Custom Field Template', 'karuta' ),
+		'reading_card' => __( 'The plugin that helps optimize the input of a custom field. This is a plugin was made in Japan.', 'karuta' ),
+		'getting_card' => 'Custom Field Template',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -65,8 +65,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '独自のテーブルやコメントなどを利用せずにWebサイトの多言語化ができるプラグイン', 'karuta' ),
-		'getting_card' => __( 'Bogo', 'karuta' ),
+		'reading_card' => __( 'The plugin that can multilingualize the Web site without using original table or comment.', 'karuta' ),
+		'getting_card' => 'Bogo',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -74,8 +74,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'WordPressが生成する動的Webページを静的ファイルとして出力するプラグイン', 'karuta' ),
-		'getting_card' => __( 'StaticPress', 'karuta' ),
+		'reading_card' => __( 'The plugin to output static files from the dynamic web pages that WordPress generates.', 'karuta' ),
+		'getting_card' => 'StaticPress',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -83,8 +83,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'ログイン時のユーザー名をメールアドレスに変更し、ブルートフォースアタック対策となるプラグイン', 'karuta' ),
-		'getting_card' => __( 'Force Email Login', 'karuta' ),
+		'reading_card' => __( 'The plugin to change the user\'s login name into the e-mail address. This might be effective for brute force attack.', 'karuta' ),
+		'getting_card' => 'Force Email Login',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -92,8 +92,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '管理画面からカスタム投稿タイプ、カスタム分類の定義を行うことができるプラグイン', 'karuta' ),
-		'getting_card' => __( 'Custom Post Type UI', 'karuta' ),
+		'reading_card' => __( 'This plugin provides an easy to use interface to create and administer custom post types and taxonomies.', 'karuta' ),
+		'getting_card' => 'Custom Post Type UI',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -101,8 +101,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'カテゴリー、タグなどの登録をラジオボタンにしたり、順序を設定できるようにするなど、タクソノミーの使い勝手を向上させるプラグイン', 'karuta' ),
-		'getting_card' => __( 'PS Taxonomy Expander', 'karuta' ),
+		'reading_card' => __( 'This plugin makes easy to use categories, tags and custom taxonomies on editing posts.', 'karuta' ),
+		'getting_card' => 'PS Taxonomy Expander',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -110,8 +110,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'Google Analyticsのデータを元にしたアクセス数ランキングプラグイン', 'karuta' ),
-		'getting_card' => __( 'Simple GA Ranking', 'karuta' ),
+		'reading_card' => __( 'This plugin provides to make access ranking by using google analytics.', 'karuta' ),
+		'getting_card' => 'Simple GA Ranking',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -119,8 +119,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '検索エンジン向けのサイトマップXMLを出力するプラグイン', 'karuta' ),
-		'getting_card' => __( 'Google XML Sitemaps', 'karuta' ),
+		'reading_card' => __( 'This plugin will generate a special XML sitemap which will help search engines.', 'karuta' ),
+		'getting_card' => 'Google XML Sitemaps',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -128,8 +128,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'FacebookやTwitterなどでのWebサービスでWordPressへのログインが可能にするプラグイン', 'karuta' ),
-		'getting_card' => __( 'Gianism', 'karuta' ),
+		'reading_card' => __( 'This plugin enables allow users to login/register with major Web service\'s accounts like Facebook, Twitter, and so on.', 'karuta' ),
+		'getting_card' => 'Gianism',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -137,8 +137,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '作成済みの記事をコピーして新しい下書きを作成するプラグイン', 'karuta' ),
-		'getting_card' => __( 'Duplicate Post', 'karuta' ),
+		'reading_card' => __( 'This plugin allows to clone a post or page, or edit it as a new draft.', 'karuta' ),
+		'getting_card' => 'Duplicate Post',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -146,8 +146,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '記事内やコメントに記述されたリンクや画像がリンク切れを起こしていないかチェックするプラグイン', 'karuta' ),
-		'getting_card' => __( 'Broken Link Checker', 'karuta' ),
+		'reading_card' => __( 'This plugin will monitor your blog looking for broken links and let you know if any are found.', 'karuta' ),
+		'getting_card' => 'Broken Link Checker',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -155,8 +155,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '画像のアップロードとリンク先の設定で、バナー掲載を簡単にするプラグイン', 'karuta' ),
-		'getting_card' => __( 'Image Widget', 'karuta' ),
+		'reading_card' => __( 'A simple image widget that uses the native WordPress media manager to add image widgets to your site.', 'karuta' ),
+		'getting_card' => 'Image Widget',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -164,8 +164,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '404エラーの検知機能なども搭載し、リダイレクトの設定を管理できるプラグイン', 'karuta' ),
-		'getting_card' => __( 'Redirection', 'karuta' ),
+		'reading_card' => __( 'This plugin manage 301 redirections, keep track of 404 errors.', 'karuta' ),
+		'getting_card' => 'Redirection',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -173,8 +173,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'Smush.it APIを用いて画像のファイルサイズを最適化するプラグイン', 'karuta' ),
-		'getting_card' => __( 'WP Smush.it', 'karuta' ),
+		'reading_card' => __( 'This plugin will reduce image file sizes and improve performance using the Smush.it API within WordPress.', 'karuta' ),
+		'getting_card' => 'WP Smush.it',
 		'meta' => 
 		array (
 			'category' => 'plugin',
@@ -182,8 +182,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'カテゴリーのアーカイブページが表示されているかを判定する', 'karuta' ),
-		'getting_card' => __( 'is_category', 'karuta' ),
+		'reading_card' => __( 'This Conditional Tag checks if a Category archive page is being displayed. ', 'karuta' ),
+		'getting_card' => 'is_category',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -191,8 +191,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '記事が特定のカテゴリーに属しているかを判定する条件分岐タグ', 'karuta' ),
-		'getting_card' => __( 'in_category', 'karuta' ),
+		'reading_card' => __( 'Tests if the current post (or any specified post) is assigned to any of the specified categories.', 'karuta' ),
+		'getting_card' => 'in_category',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -200,8 +200,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '時系列で次の投稿となるリンクを表示する', 'karuta' ),
-		'getting_card' => __( 'next_post_link', 'karuta' ),
+		'reading_card' => __( 'Used on single post permalink pages, this template tag displays a link to the next post which exists in chronological order from the current post.', 'karuta' ),
+		'getting_card' => 'next_post_link',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -209,8 +209,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '次の記事一覧ページのリンクを表示する', 'karuta' ),
-		'getting_card' => __( 'next_posts_link', 'karuta' ),
+		'reading_card' => __( 'Prints a link to the next set of posts within the current query.', 'karuta' ),
+		'getting_card' => 'next_posts_link',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -218,8 +218,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'ウィジェットエリアのウィジェットが登録されているかを判定する条件分岐タグ', 'karuta' ),
-		'getting_card' => __( 'is_active_sidebar', 'karuta' ),
+		'reading_card' => __( 'This Conditional Tag checks if a given sidebar is active (in use). This is a boolean function, meaning it returns either TRUE or FALSE.', 'karuta' ),
+		'getting_card' => 'is_active_sidebar',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -227,8 +227,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'ヘッダー、またはフッターにJavaScriptファイルへのリンク要素出力を登録する', 'karuta' ),
-		'getting_card' => __( 'wp_enqueue_script', 'karuta' ),
+		'reading_card' => __( 'Register the link element output to a JavaScript file with a header or a footer', 'karuta' ),
+		'getting_card' => 'wp_enqueue_script',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -236,8 +236,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'headタグ内に必須となるテンプレートタグ、CSSやJavaScriptのlink要素などを出力する', 'karuta' ),
-		'getting_card' => __( 'wp_head', 'karuta' ),
+		'reading_card' => __( 'Output the link elements of a template tag becoming required, CSS and the JavaScript in head tag', 'karuta' ),
+		'getting_card' => 'wp_head',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -245,8 +245,9 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '記事の作成者名を表示する', 'karuta' ),
-		'getting_card' => __( 'the_author', 'karuta' ),
+		'reading_card' => 'Display a creator name of the article
+',
+		'getting_card' => 'the_author',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -254,8 +255,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '投稿タイプの定義を行う。initフックで行う必要がある', 'karuta' ),
-		'getting_card' => __( 'register_post_type', 'karuta' ),
+		'reading_card' => __( 'Define the post type. It is necessary to perform it with init hook', 'karuta' ),
+		'getting_card' => 'register_post_type',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -263,8 +264,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '表示するページ種類に応じたclass名を表示する', 'karuta' ),
-		'getting_card' => __( 'body_class', 'karuta' ),
+		'reading_card' => __( 'Display a class name depending on kinds of the page', 'karuta' ),
+		'getting_card' => 'body_class',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -272,8 +273,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'ループ中の記事の内容に応じたclass名を出力する', 'karuta' ),
-		'getting_card' => __( 'post_class', 'karuta' ),
+		'reading_card' => __( 'Output a class name depending on the content of an article in a loop', 'karuta' ),
+		'getting_card' => 'post_class',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -281,8 +282,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'サイトアドレスを取得する', 'karuta' ),
-		'getting_card' => __( 'home_url', 'karuta' ),
+		'reading_card' => __( 'Acquire site address', 'karuta' ),
+		'getting_card' => 'home_url',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -290,8 +291,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'マルチサイトにおいて、引数で指定したサイトに切り替えを行う', 'karuta' ),
-		'getting_card' => __( 'switch_to_blog', 'karuta' ),
+		'reading_card' => __( 'In a multi-site, it change to the site that appointed with an argument', 'karuta' ),
+		'getting_card' => 'switch_to_blog',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -299,8 +300,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'テーマ関連機能を有効化する', 'karuta' ),
-		'getting_card' => __( 'add_theme_support', 'karuta' ),
+		'reading_card' => __( 'Allows a theme or plugin to register support of a certain theme feature', 'karuta' ),
+		'getting_card' => 'add_theme_support',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -308,8 +309,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'ビジュアルエディターに特定のCSSファイルを読み込む', 'karuta' ),
-		'getting_card' => __( 'add_editor_style', 'karuta' ),
+		'reading_card' => __( 'Allows theme developers to link a custom stylesheet file to the TinyMCE visual editor', 'karuta' ),
+		'getting_card' => 'add_editor_style',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -317,8 +318,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'WordPressループの内部カウンタをインクリメントし、ループ内で表示する記事情報のセットアップを行う', 'karuta' ),
-		'getting_card' => __( 'the_post', 'karuta' ),
+		'reading_card' => __( 'Iterate the post index in The Loop. Retrieves the next post, sets up the post, sets the \'in the loop\' property to true.', 'karuta' ),
+		'getting_card' => 'the_post',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -326,8 +327,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '表示する記事の有無を判定する', 'karuta' ),
-		'getting_card' => __( 'have_posts', 'karuta' ),
+		'reading_card' => __( 'This function checks to see if the current WordPress query has any results to loop over. This is a boolean function, meaning it returns either TRUE or FALSE.', 'karuta' ),
+		'getting_card' => 'have_posts',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -335,8 +336,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'XSSの防止に表示されるテキストをエスケープ処理を行う', 'karuta' ),
-		'getting_card' => __( 'esc_html', 'karuta' ),
+		'reading_card' => __( 'Work to escape a displayed text. To prevent XSS. ', 'karuta' ),
+		'getting_card' => 'esc_html',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -344,8 +345,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( '時系列で１つ前となる投稿へのリンクを表示する', 'karuta' ),
-		'getting_card' => __( 'previous_post_link', 'karuta' ),
+		'reading_card' => __( 'This template tag displays a link to the previous post which exists in chronological order from the current post.', 'karuta' ),
+		'getting_card' => 'previous_post_link',
 		'meta' => 
 		array (
 			'category' => 'template tag',
@@ -353,8 +354,8 @@ $cards = array (
 	),
 	 
 	array (
-		'reading_card' => __( 'アーカイブページにおいて、ページ送り数が少ないページへのリンクを表示する', 'karuta' ),
-		'getting_card' => __( 'previous_posts_link', 'karuta' ),
+		'reading_card' => __( 'Prints a link to the next set of posts within the current query.', 'karuta' ),
+		'getting_card' => 'previous_posts_link',
 		'meta' => 
 		array (
 			'category' => 'template tag',
